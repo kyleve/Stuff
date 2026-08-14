a1ddcb939c53423f79e006f4be0f1f78b898e62d
Kyle Van Essen <k@kve.me>
2026-08-13 21:44:34 -0700

    Remove redundant passport smoke tests

--- Where/WhereUI/Tests/AboutOpenSourceFooterTests.swift
+++ Where/WhereUI/Tests/AboutOpenSourceFooterTests.swift
@@ -1,19 +1,9 @@
-import SwiftUI
-import TestHostSupport
 import Testing
 @testable import WhereUI
 
-@MainActor
 struct AboutOpenSourceFooterTests {
     @Test func linksToTheCanonicalProjectRepository() {
         #expect(AboutOpenSourceFooter.projectURL
             .absoluteString == "https://github.com/kyleve/Stuff")
-    }
-
-    @Test func hosts() throws {
-        let rootView = AboutOpenSourceFooter().whereBroadwayRoot()
-        try show(UIHostingController(rootView: rootView)) { hosted in
-            #expect(hosted.view != nil)
-        }
     }
 }
